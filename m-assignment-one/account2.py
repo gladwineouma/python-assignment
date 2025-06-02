48a8148dfb12a169c4f356dbbe7248e42f2306e3
Student <oumagladwineesther@gmail.com>
2025-06-02 22:09:29 +0300

    assignment done

--- m-assignment-one/account2.py
+++ m-assignment-one/account2.py
@@ -32,14 +32,14 @@ class Account():
             print(f"Hello {self.name} you have {narration} {amount} your new balance is {self._balance} on {transaction.date}")
 
 
-    def withdraw(self,amount,narration = "withdrawals"):
+    def get_withdraw(self,amount,narration = "withdrawals"):
         current_date = datetime.datetime.now()
         if amount > self._balance:
-            return 'insufficient balance'
+            return "insufficient balance"
         if self._balance-amount < self.minimum_balance:
-            return 'Can not withdraw,amount is more than minimun balance'
+            return "Can not withdraw,amount is more than minimun balance"
         self._balance -= amount
-        transaction = Transaction(current_date,narration,'debits',amount)
+        transaction = Transaction(current_date,narration,"debits",amount)
         self.transactions.append(transaction)
         print(f"Withdraw:{amount}:New balance {self._balance} on {transaction.date}")    
         
@@ -55,13 +55,13 @@ class Account():
         self.withdraw(amount)
         other_account.deposit(amount)
         self._balance -= amount
-        transaction = Transaction(current_date,narration,'debits',amount)
+        transaction = Transaction(current_date,narration,"debits",amount)
         self.transactions.append(transaction)
         return f'You have transferred {amount} to {other_account.name} :New balance {self._balance} on {transaction.date}'
 
 
 
-    def request_loan(self,amount,narration = 'requested_loan'):
+    def request_loan(self,amount,narration = "requested_loan"):
         current_date = datetime.datetime.now()
         if self.is_frozen :
             return "Account is frozen,your can not transfer money"
@@ -69,7 +69,7 @@ class Account():
             return "You don't have enough amount in your account"
         self.loan_amount+=amount
         self._balance += amount
-        transaction = Transaction(current_date,narration,'loan',amount)
+        transaction = Transaction(current_date,narration,"loan",amount)
         self.transactions.append(transaction)
         return f'Loan requested:{amount}.New balance:{self._balance}'
 
@@ -84,32 +84,34 @@ class Account():
         excess_money += amount
         self._balance -= amount
         self.loan_amount -= amount
-        transaction = Transaction(current_date,narration,'repaid_loan',amount)
+        transaction = Transaction(current_date,narration,"repaid_loan",amount)
         self.transactions.append(transaction)
-        return f'You repaid:{amount}.Remaining amount:{self.loan_amount}'
+        return f"You repaid:{amount}.Remaining amount:{self.loan_amount}"
 
 
         
-    def compute_balance(self):
+    def get_balance(self):
         balance = 0
         for transaction in self.transactions:
-            if transaction.transaction_type == 'credits':
+            if transaction.transaction_type == "credit":
                 balance += transaction.amount
-            elif transaction.transaction_type == 'debits':
+            elif transaction.transaction_type == "debits":
                 balance -= transaction.amount
-            elif transaction.transaction_type == 'debits':
+            elif transaction.transaction_type == "debits":
                 balance -= transaction.amount
-            elif transaction.transaction_type == 'loan':
+            elif transaction.transaction_type == "loan":
                 balance+= transaction.amount
-            else: transaction.transaction_type == 'repay_loan'
+            else: transaction.transaction_type == "repay_loan"
             balance -= amount
         return balance
+
+
     def show_balance(self):
-        return f"current balance:{self.compute_balance()}"
+        return f"current balance:{self.get_balance()}"
 
 
     def account_details(self):
-        return f'Account owner:{self.name}.current balance:{self._balance}.Loan amount:{self.loan_amount}'
+        return f"Account owner:{self.name}.current balance:{self._balance}.Loan amount:{self.loan_amount}"
 
 
     def change_account_owner(self, new_owner):
@@ -125,7 +127,7 @@ class Account():
         interest = self._balance * 0.05
         self._balance += interest
         self.transactions.append(interest)
-        return f'interest {interest}.New balance:{self._balance}'
+        return f"interest {interest}.New balance:{self._balance}"
 
 
     def freeze_account(self):
@@ -135,17 +137,17 @@ class Account():
 
     def unfreeze_account (self):
         self.is_frozen = False
-        return 'Account has been unfrozen'
+        return "Account has been unfrozen"
 
 
     def set_minimum_balance (self):
         if amount <= 0:
-            return 'minimum value must be positive numbers'
+            return "minimum value must be positive numbers"
         self.minimum_balance = 200
-        return f'minimum balance set to:{self.minimun_balance}'
-
+        return f"minimum balance set to:{self.minimun_balance}
+"
 
     def close_account(self):
         self._balance = 0
         self.transactions.clear()
-        return 'Account closed .All balances set to zero and transactions cleared'
+        return "Account closed"
